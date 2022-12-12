ba384fc6778f31a7217bdafde46739fd14bcf10d
shuhad <shuhad.loofer@gmail.com>
2022-12-12 15:05:44 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
+create database vet_clinic
 
-CREATE TABLE animals (
-    name varchar(100)
+create table animals
+(
+	Id integer primary key,
+	Name varchar(50),
+	Date_of_birth date,
+	Escape_attempts integer,
+	Neutered boolean,
+	Weight_kg real
 );
