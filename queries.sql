780b3ddfe719e2bced1d144fdf9c4855b4641744
shuhad <shuhad.loofer@gmail.com>
2022-12-12 17:59:07 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name LIKE 'mon%';
+SELECT * from animals WHERE name LIKE '%mon';
 
 SELECT name from animals WHERE date_of_birth BETWEEN '01-01-2016' AND '01-01-2019';
 
